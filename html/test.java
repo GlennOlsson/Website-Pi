6d9abb8f1a15b8ae623fba48f2aa618aea6e3e9b
Glenn <glennholsson@gmail.com>
2017-03-14 21:53:06 +0100

    Testing jsp

---
+++ html/test.java
@@ -0,0 +1,7 @@
+public class test {
+
+  public String getString(){
+    return "Heyya, bitch";
+  }
+  
+}
---
+++ html/test.jsp
@@ -0,0 +1,8 @@
+<%
+
+String temp1;
+test obj = new test();
+temp1 = obj.getString();
+%>
+
+<%=temp1 %>
