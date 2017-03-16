fa69521d9c07339974b3fabb5ff1c0c616b5e79f
Glenn <glennholsson@gmail.com>
2017-03-16 14:15:33 +0100

    s

--- html/Test.java
+++ html/Test.java
@@ -2,7 +2,7 @@ public class Test {
   public static void main(String args[])
      {
         Test obj = new Test();
-         String temp1= obj.getString();
+         String temp1= obj.getString("mutha", "fucka");
      }
   public String getString(String hey, String yo){
     return "Heyya, bitch " + yo + "  --  " +hey;
