98430e156b51a596e99046def269db3c2316f08d
Glenn Olsson <glennol@kth.se>
2017-11-24 13:02:05 +0100

    Fixed ifames for Print.md and Errorlog.md

---
+++ html/api.java
@@ -0,0 +1,20 @@
+public class api{
+
+    public static void main(String[] args) {
+        new api();
+    }
+
+    public api(){
+        int portNumber = 4444;
+
+    try (
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        Socket clientSocket = serverSocket.accept();
+        PrintWriter out =
+            new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+        new InputStreamReader(clientSocket.getInputStream()));
+    ) {
+    }
+
+}
