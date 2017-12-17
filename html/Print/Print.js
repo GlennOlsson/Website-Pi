797477260d2c3bdc76ccbb159eb230fecb113306
Glenn Olsson <glennol@kth.se>
2017-12-17 16:48:12 +0100

    New way to parse file content

--- html/Print/Print.js
+++ html/Print/Print.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
 
-    var client = new XMLHttpRequest();
-    client.open('GET', '../Print.md');
-    client.onreadystatechange = function() {
-        $("p").text(client.responseText)
-        $(".scrollingDiv").scrollTop( 300 );
-    }
-    client.send();
+    $.get('../Print.md', function(data) {
+   $("p").text(data)
+}, 'text');
 
 
 });
