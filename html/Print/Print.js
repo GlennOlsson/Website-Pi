a3f6d669956e0405753c3e4fcbbfec8009f0f402
Glenn Olsson <glennol@kth.se>
2017-12-17 17:25:33 +0100

    Fixing fixing...

--- html/Print/Print.js
+++ html/Print/Print.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
     function updateFromFile(){
         $.get('../Print.md', function(data) {
             $("p").text(data)
+
+            $("html, body").animate({
+                scrollTop: $(document).height()
+            }, 1);
         }, "text");
     }
 
     updateFromFile()
-    $("html, body").animate({
-         scrollTop: $(document).height()
-    }, 1);
+
 
     /*
     var millisecondsToWait = 1500;
