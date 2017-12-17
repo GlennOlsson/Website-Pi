038ff77213b093a073793c1b36648d2113db6480
Glenn Olsson <glennol@kth.se>
2017-12-17 18:01:48 +0100

    Fixed the new print format to Errorog as well

---
+++ html/Errorlog/Errorlog.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    function updateFromFile(){
+        $.get('../Errorlog.md', function(data) {
+            $("p").text(data)
+
+            $("html, body").animate({
+                scrollTop: $(document).height()
+            }, 1);
+
+        }, "text");
+    }
+
+    updateFromFile()
+
+    $(".updateButton").click(function(){
+        $(this).prop('disabled', true);
+        updateFromFile()
+        $(this).prop('disabled', false);
+    })
+
+});
