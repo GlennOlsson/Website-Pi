39b870137ba79c071c368e8191be40c637ad6e7f
Glenn Olsson <glennol@kth.se>
2018-01-08 20:25:18 +0100

    Mobile fitted page as well. Code is ugly as hell though

---
+++ html/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    var md = new MobileDetect(window.navigator.userAgent);
+
+    var mobile = md.mobile();
+    if (mobile == null) {
+        console.log("Not Mobile")
+    }
+    else{
+        console.log("Mobile")
+        $(".background-image").attr("src", "")
+        $(".background-image").attr("width", "")
+        $(".background-image").attr("height", "100%")
+
+        $(".background-div").attr("id", "vertical")
+        $(".background-div").css("background-image", "url(Resources/StockholmVertical.png)")
+
+        $(".logos").css("margin-top", "50%")
+        $(".logos").css("margin-left", "29%")
+}
+});
