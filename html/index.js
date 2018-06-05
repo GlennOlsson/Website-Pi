e76e3428c260140da678542bd8f6e0af4863eee1
Glenn Olsson <glennol@kth.se>
2018-06-05 18:14:14 +0200

    Also better looking for mobile now

--- html/index.js
+++ html/index.js
@@ -8,17 +8,7 @@ $(document).ready(function(){
     }
     else{
         console.log("Mobile")
-        $(".background-image").attr("src", "")
-        $(".background-image").attr("width", "")
-        $(".background-image").attr("height", "100%")
-
-        $(".background-div").attr("id", "vertical")
-        $(".background-div").css("background-image", "url(Resources/StockholmVertical.png)")
-
-        $(".logos").css("margin-top", "50%")
-        $(".logos").css("margin-left", "29%")
-
-        $(".GlennText").css("margin-top", "20%")
-        $(".GlennText").css("margin-left", "24%")
-}
+        $("body").css("background-image", "url(\"Resources/STHLMVertical-1510x1835.jpg\")")
+        $("body").css("background-size", "200%")
+    }
 });
